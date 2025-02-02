15f74b3f6b625fef004498220865af9c9ba1aecc
Oviemena <okpowoduoviemena999@gmail.com>
2025-02-02 23:42:57 +0100

    Remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-  // app.useGlobalGuards(new JwtAuthGuard());
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
